18c6b099c22a6862f52f79a5be2214595c4318d4
Ivan-Nieto <ivan@inieto.dev>
2021-08-27 10:56:44 -0600

    Added migration for user entity

---
+++ src/database/migrations/1630082989749-user.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class user1630082989749 implements MigrationInterface {
+    name = 'user1630082989749'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "age" integer NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "user"`);
+    }
+
+}
